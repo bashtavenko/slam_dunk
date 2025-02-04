dc1c385a5542baebd7a0a30652dea91405659481
bashtavenko <stanbpublic@live.com>
2025-02-04 11:12:15 -0500

    proto_utils completed

--- proto_utils_test.cc
+++ proto_utils_test.cc
@@ -1,12 +1,20 @@
 #include "proto_utils.h"
 #include <google/protobuf/text_format.h>
+#include "absl/status/status_matchers.h"
 #include "gmock/gmock-matchers.h"
 #include "gtest/gtest.h"
 #include "lidar.h"
 #include "proto/lidar_response.pb.h"
+#include "tools/cpp/runfiles/runfiles.h"
 
 namespace slam_dunk {
 namespace {
+
+using ::absl_testing::IsOk;
+using ::absl_testing::IsOkAndHolds;
+using ::bazel::tools::cpp::runfiles::Runfiles;
+using ::testing::HasSubstr;
+using ::testing::NotNull;
 
 TEST(ScanResponseToTextProtoString, Works) {
   auto text_proto =
@@ -18,6 +26,20 @@ TEST(ScanResponseToTextProtoString, Works) {
   slam_dunk::proto::ScanResponse message;
   ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(text_proto.value(),
                                                             &message));
+}
+
+TEST(SaveAndGetFile, Works) {
+  const Runfiles* files = Runfiles::CreateForTest();
+  ASSERT_THAT(files, NotNull());
+  const std::string test_file = files->Rlocation("lidar.txtpb");
+  EXPECT_THAT(
+      SaveToFile(
+          std::vector<ScanResponse>{
+              ScanResponse{.theta = 5566, .distance_mm = 2257, .quality = 60},
+              ScanResponse{.theta = 5822, .distance_mm = 2243, .quality = 60}},
+          test_file),
+      IsOk());
+  EXPECT_THAT(GetTextFromFile(test_file), IsOkAndHolds(HasSubstr("items")));
 }
 
 }  // namespace
